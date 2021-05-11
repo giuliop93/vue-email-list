98525028e1a40012c962bb0e3703f1058c6cf8e3
Giulio_Pa <giuliop93@hotmail.it>
2021-05-11 16:34:35 +0200

    wip

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const app = new Vue({
         mailingList: []
     },
     mounted() {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(function(resp){
-            console.log(resp);
-            this.mailingList.push(resp.data.response)
-        });
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    console.log(resp);
+                    this.mailingList.push(resp.data.response)
+                });
+        }
     },
 })
